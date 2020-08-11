947982d550824c22997201db8acdb8ff341b263b
Fredrik Vraalsen <fredrik@vraalsen.no>
2020-08-11 15:48:44 +0200

    DP-1035 Don't set 's3path' if not present (to get sparse index)

--- status/StatusData.py
+++ status/StatusData.py
@@ -16,40 +16,28 @@ class StatusData:
         return str(uuid.uuid4())
 
     def create_item(self, body):
-        application = body["application"]
         application_id = body["application_id"]
-        handler = body["handler"]
         status_row_id = self.generate_uuid(application_id)
         event_id = self.generate_event_uuid()
 
-        user = body["user"]
-        date_started = body["date_started"]
-        date_end = body["date_end"]
-        run_status = "STARTED"
-        status = "OK"
-        status_body = body["body"]
-        s3path = "N/A"
+        item = {
+            "id": status_row_id,
+            "event_id": event_id,
+            "application": body["application"],
+            "user": body["user"],
+            "date_started": body["date_started"],
+            "date_end": body["date_end"],
+            "run_status": "STARTED",
+            "status": "OK",
+            "status_body": body["body"],
+            "application_id": application_id,
+            "handler": body["handler"],
+            "meta": body.get("meta", "N/A"),
+        }
         if "s3path" in body:
-            s3path = body["s3path"]
-        meta = body.get("meta", "N/A")
+            item["s3path"] = body["s3path"]
 
-        db_response = self.table.put_item(
-            Item={
-                "id": status_row_id,
-                "event_id": event_id,
-                "application": application,
-                "user": user,
-                "date_started": date_started,
-                "date_end": date_end,
-                "run_status": run_status,
-                "status": status,
-                "status_body": status_body,
-                "application_id": application_id,
-                "handler": handler,
-                "s3path": s3path,
-                "meta": meta,
-            }
-        )
+        db_response = self.table.put_item(Item=item)
 
         if db_response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             return status_row_id
