aeeb5ce99960e3a603e1ab220a5dd1822ba87763
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2021-04-26 20:18:17 +0200

    DP-1845: Delete is_owner() from common

--- status/common.py
+++ status/common.py
@@ -29,24 +29,6 @@ def _is_dataset_owner(token, dataset_id):
     return data.get("access", False)
 
 
-def is_owner(event, item):
-    # TODO: Fallback to "domain" for backwards compatibility.
-    # Expect "application" once clients are upadted.
-    headers = event.get("headers")
-    if headers:
-        auth_header = event["headers"].get("Authorization", "")
-        bearer_token = auth_header.split(" ")[-1]
-        if item.get("application") == "dataset":
-            dataset_id = item["application_id"]
-            return _is_dataset_owner(bearer_token, dataset_id)
-
-        if item["domain"] == "dataset":
-            dataset_id = item["domain_id"].split("/")[0]
-            return _is_dataset_owner(bearer_token, dataset_id)
-
-    return False
-
-
 def extract_bearer_token(event):
     auth_header = event["headers"].get("Authorization", "")
     return auth_header.split(" ")[-1]
