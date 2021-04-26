5fa2c1098ea2c0726bb3861cc17701a37d10056e
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2021-04-26 20:17:33 +0200

    DP-1845: Use ResourceAuthorizer in update_status

--- status/update_status.py
+++ status/update_status.py
@@ -1,11 +1,18 @@
 import simplejson
 from aws_xray_sdk.core import patch_all, xray_recorder
 from okdata.aws.logging import logging_wrapper, log_add
+from okdata.resource_auth import ResourceAuthorizer
 
 from status.StatusData import StatusData
-from status.common import response, response_error, is_owner
+from status.common import (
+    response,
+    response_error,
+    extract_dataset_id,
+    extract_bearer_token,
+)
 
 patch_all()
+resource_authorizer = ResourceAuthorizer()
 
 
 @logging_wrapper
@@ -24,10 +31,16 @@ def handler(event, context):
         error = f"Could not find the requested item to update: {trace_id}"
         return response_error(404, error)
 
-    items = result["Items"]
-    caller_is_owner = is_owner(event, items[0])
-    log_add(is_owner=caller_is_owner)
-    if not caller_is_owner:
+    status_items = result["Items"]
+    dataset_id = extract_dataset_id(status_items[0])
+    bearer_token = extract_bearer_token(event)
+
+    if not resource_authorizer.has_access(
+        bearer_token,
+        "okdata:dataset:write",
+        f"okdata:dataset:{dataset_id}",
+        use_whitelist=True,
+    ):
         return response_error(403, "Access denied")
     item = db.update_status(trace_id, content)
     return response(200, simplejson.dumps(item))
