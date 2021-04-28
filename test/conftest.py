edabfa0b62e5dd0eeb452e1d9f664aac9bf897c4
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2021-04-28 10:06:46 +0200

    DP-1845: Use smarter syntax in has_access_mock

--- test/conftest.py
+++ test/conftest.py
@@ -9,13 +9,11 @@ def mock_auth(monkeypatch):
     def has_access_mock(
         self, bearer_token, scope, resource_name=None, use_whitelist=False
     ):
-        if (
+        return (
             bearer_token == test_data.bearer_token_with_access
             and resource_name.startswith("okdata:dataset:")
             and scope == "okdata:dataset:write"
             and use_whitelist
-        ):
-            return True
-        return False
+        )
 
     monkeypatch.setattr(ResourceAuthorizer, "has_access", has_access_mock)
