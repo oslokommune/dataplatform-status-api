039c7518d9f6c229c1c333d6c197739465fee520
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2021-05-19 15:06:50 +0200

    Remove unused method

--- status/common.py
+++ status/common.py
@@ -1,6 +1,5 @@
 import os
 
-import requests
 import simplejson
 
 AUTHORIZER_API = os.environ["AUTHORIZER_API"]
@@ -17,16 +16,6 @@ def response(code, body):
 def response_error(code, message):
     body = {"message": message}
     return http_response(code, simplejson.dumps(body))
-
-
-def _is_dataset_owner(token, dataset_id):
-    result = requests.get(
-        f"{AUTHORIZER_API}/{dataset_id}",
-        headers={"Authorization": f"Bearer {token}"},
-    )
-    result.raise_for_status()
-    data = result.json()
-    return data.get("access", False)
 
 
 def extract_bearer_token(event):
